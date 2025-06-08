9cd1bac0948922aaa9bee971264ac7113e3000e5
Ernestas Grubis <ernestasg02@gmail.com>
2025-06-08 19:45:41 +0300

    Remove console log from env-utils and add frontend metadata to package.json

--- frontend/src/lib/utils/env-utils.ts
+++ frontend/src/lib/utils/env-utils.ts
@@ -4,7 +4,6 @@ const clientEnvSchema = z.object({
     NEXT_PUBLIC_GATEWAY_DOMAIN: z.string().url(),
 });
 
-console.log(process.env);
 export const env = clientEnvSchema.parse({
     NEXT_PUBLIC_GATEWAY_DOMAIN: process.env.NEXT_PUBLIC_GATEWAY_DOMAIN,
 });
